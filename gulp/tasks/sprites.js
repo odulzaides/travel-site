6254fcbec623dce9d8f2dd6240468a3e28570e47
odulzaides <dulzaides.oscar@gmail.com>
2016-11-26 16:25:21 -0500

    Fetaure: sprite file for icons

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     svgSprite = require('gulp-svg-sprite'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    del = require('del');
 
 var config = {
     mode: {
@@ -14,21 +15,36 @@ var config = {
         }
     }
 }
-gulp.task('createSprite', function () {
+
+// Clean out old folders
+gulp.task('beginClean', function () {
+    return del(['./app/temp/sprite', './app/assets/images/sprites'])
+});
+
+// Make a sprite file from icon or image files
+gulp.task('createSprite', ['beginClean'], function () {
     return gulp.src('./app/assets/images/icons/**/*.svg')
         .pipe(svgSprite(config))
         .pipe(gulp.dest('./app/temp/sprite/'));
 });
 
+// Copy SVG to corresct location in assets/images/sprites folder
 gulp.task('copySpriteGraphic', ['createSprite'], function () {
     return gulp.src('./app/temp/sprite/css/**/*.svg')
         .pipe(gulp.dest('./app/assets/images/sprites/'))
 });
 
+// Copy styles file to the modules so icons can be used
 gulp.task('copySpriteCSS', ['createSprite'], function () {
     return gulp.src('./app/temp/sprite/css/*.css')
         .pipe(rename('_sprite.css'))
         .pipe(gulp.dest('./app/assets/styles/modules/'))
 });
 
-gulp.task('icons', ['createSprite', 'copySpriteGraphic', 'copySpriteCSS']);
+// Clean up /app/temp folder
+gulp.task('endClean', ['copySpriteCSS'], function () {
+    return del(['./app/temp/sprite'])
+});
+
+// Main icon task to trigger process 
+gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteGraphic', 'copySpriteCSS', 'endClean']);

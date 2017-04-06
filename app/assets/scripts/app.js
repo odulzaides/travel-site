51d62ecac82bf9b648c2b413b766a9b8427e547a
odulzaides <dulzaides.oscar@gmail.com>
2017-04-06 16:31:38 -0700

    Feature: Sticky header background color darkening

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,7 +1,9 @@
 import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from '../scripts/modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 
 const test = new MobileMenu();
 new RevealOnScroll($('.feature-item'), "85%");
 new RevealOnScroll($('.testimonial'), '60%');
+var stickyHeader = new StickyHeader();
